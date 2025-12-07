46be9bdc5592f2a5cd901f736d9da4b8726ca230
ahaNoDobre <gubany1@stud.uniza.sk>
2025-12-07 17:05:34 +0100

    Fixed hamburger menu.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,7 @@
 
     {{-- Vite pre kompiláciu CSS/JS (momentálne zakázané) --}}
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 </head>
 {{-- Svetlo-šedé pozadie celej stránky --}}
